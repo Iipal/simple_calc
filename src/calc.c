713310cfb71df1fe1706414373b89550a97eca27
Iipal <malkoleyplay@gmail.com>
2020-02-18 04:19:24 +0200

    init calc

---
+++ src/calc.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+
+bool	syntax_parser(const char *expr);
+bool	syntax_parser(const char *expr) {
+	return !!expr;
+}
+
+int	main(int argc, char *argv[]) {
+	--argc; ++argv;
+	if (!argc)
+		errx(EXIT_FAILURE, "Usage: ./calc \"<expr1>\" ... \"<exprN>\"");
+
+	while (argc) {
+		if (!syntax_parser(*argv))
+			return EXIT_FAILURE;
+		--argc; ++argv;
+	}
+}
